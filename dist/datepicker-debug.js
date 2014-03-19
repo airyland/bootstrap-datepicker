d96b936a479ed0a3e1c6d377b3953133cd590823
airyland <airyland@qq.com>
2014-03-19 14:44:17 +0800

    rebuild

--- dist/datepicker-debug.js
+++ dist/datepicker-debug.js
@@ -1,4 +1,4 @@
-define("seedit/datepicker/0.0.1/datepicker-debug", [], function(require, exports, module) {
+define("moe/datepicker/0.0.1/datepicker-debug", [], function(require, exports, module) {
     /* =========================================================
      * bootstrap-datepicker.js
      * Repo: https://github.com/eternicode/bootstrap-datepicker/
